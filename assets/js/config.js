4050f5d1346e48f34343b4abf422e0b6f0df0104
joverixal <joverixal.entuna@gmail.com>
2026-03-01 22:28:07 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxnCO-VC3HbHQKzXIuZQU0Zsyqz67qBEdj2m9f7y1FOJQveJZwGp66T5OKnppgqKb5W/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbw-Z20_-MWSKRjGWCXdFdjK533FjJ9hudBipEQYe3j8DngObXgNr6nrQIiXBWypnH-O/exec";
